e713da4c2d03ba1dd5062bba2c5f02606463a187
mavicponiente <marivic_poniente03@yahoo.com>
2021-09-04 18:59:58 +0800

    Add humidity forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,27 +1,30 @@
-const request = require("request");
+const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
   const url =
-    "http://api.weatherstack.com/current?access_key=7f8e59a8002764baf4f863d1216af59f&query=" +
+    'http://api.weatherstack.com/current?access_key=7f8e59a8002764baf4f863d1216af59f&query=' +
     longitude +
-    "," +
+    ',' +
     latitude +
-    " &units=f";
+    ' &units=f';
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
-      callback("Unable to connect to weather service!", undefined);
+      callback('Unable to connect to weather service!', undefined);
     } else if (body.error) {
-      callback("Unable to find locations!", undefined);
+      callback('Unable to find locations!', undefined);
     } else {
       callback(
         undefined,
         body.current.weather_descriptions[0] +
-          ". It is currently " +
+          '. It is currently ' +
           body.current.temperature +
-          " degress out. It feels like " +
+          ' degress out. It feels like ' +
           body.current.feelslike +
-          " degrees out. "
+          ' degrees out. ' +
+          'The humidity is at ' +
+          body.current.humidity +
+          ' degrees celsius.'
       );
     }
   });
